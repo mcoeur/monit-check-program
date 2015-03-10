43f4aa73c8b62d29c68a7baa0074bc6213d9fda0
Mallory Coeur <mallory.coeur@gmail.com>
2015-03-10 16:12:09 +0100

    new config variable : SECURITY_UPDATES_THRESHOLD

--- system/security-updates.sh
+++ system/security-updates.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+
+DIR=$(cd "$(dirname $0)" && pwd)
+source ${DIR}/../config.cfg
+
 UPDATECOUNT=`cat /var/lib/update-notifier/updates-available | grep security | cut -d\  -f 1`
-LIMIT=2
+echo "threshold : "${SECURITY_UPDATES_THRESHOLD}
 
 if [ -z $UPDATECOUNT ] ; then
 	UPDATECOUNT=0;
 fi
 
-if  [ $UPDATECOUNT -ge $LIMIT ]; then
+if  [ $UPDATECOUNT -ge ${SECURITY_UPDATES_THRESHOLD} ]; then
         echo "Security update queue exceeded limit ($UPDATECOUNT)"
 	cat /var/lib/update-notifier/updates-available
         exit 1;
