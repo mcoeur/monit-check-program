210ed7ac8ebf1024f1325ff05afc71b713fd56e6
root <root@ns321724.ip-188-165-216.eu>
2015-04-29 12:24:23 +0200

    optimization

--- mysql/slow_queries.sh
+++ mysql/slow_queries.sh
@@ -4,10 +4,14 @@ DIR=$(cd "$(dirname $0)" && pwd)
 source ${DIR}/../config.cfg
 
 QUERY="SELECT * FROM INFORMATION_SCHEMA.PROCESSLIST WHERE USER='${MYSQL_USER}' AND COMMAND != 'Sleep' AND TIME > ${MYSQL_SLOW_QUERY_THRESHOLD} "
-QUERYCOUNT=`echo "$QUERY" | mysql -u root -p${MYSQL_PASSWORD} | wc -l`
+LIST=`echo "$QUERY" | mysql -u root -p${MYSQL_PASSWORD}`
+QUERYCOUNT=`echo "$LIST" | sed /^$/d | wc -l`
+
 if [ $QUERYCOUNT -gt 0 ] ; then
+	QUERYCOUNT=`expr $QUERYCOUNT - 1`
 	echo "Slow queries found ($QUERYCOUNT)" 1>&2
-	echo "$QUERY" | mysql -u root -p${MYSQL_PASSWORD} 1>&2
+	echo "$QUERY"
+	echo "$LIST"
 	exit 1
 else
 	echo "No slow query found"
