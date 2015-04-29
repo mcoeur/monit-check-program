7b36851b9913ae0329699ac25d6a7d62d420b451
root <root@ns321724.ip-188-165-216.eu>
2015-04-29 09:03:32 +0200

    fixes variable name

--- mysql/slow_queries.sh
+++ mysql/slow_queries.sh
@@ -7,7 +7,7 @@ QUERY="SELECT * FROM INFORMATION_SCHEMA.PROCESSLIST WHERE USER='${MYSQL_USER}' A
 QUERYCOUNT=`echo "$QUERY" | mysql -u root -p${MYSQL_PASSWORD} | wc -l`
 if [ $QUERYCOUNT -gt 0 ] ; then
 	echo "Slow queries found ($QUERYCOUNT)" 1>&2
-	echo "$QUERY" | mysql -u root -p$PASSWORD 1>&2
+	echo "$QUERY" | mysql -u root -p${MYSQL_PASSWORD} 1>&2
 	exit 1
 else
 	echo "No slow query found"
