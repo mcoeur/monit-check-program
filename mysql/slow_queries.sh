48ba419ab062ac51e41567ddb8af5a9e0c7b7b22
mcoeur <mallory.coeur@gmail.com>
2015-07-27 13:17:26 +0200

    Update slow_queries

--- mysql/slow_queries.sh
+++ mysql/slow_queries.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-
 DIR=$(cd "$(dirname $0)" && pwd)
 source ${DIR}/../config.cfg
 
-QUERY="SELECT * FROM INFORMATION_SCHEMA.PROCESSLIST WHERE USER='${MYSQL_USER}' AND COMMAND != 'Sleep' AND TIME > ${MYSQL_SLOW_QUERY_THRESHOLD} "
+QUERY="SELECT * FROM INFORMATION_SCHEMA.PROCESSLIST WHERE USER='${MYSQL_USER}' AND COMMAND != 'Sleep' AND TIME > ${MYSQL_SLOW_QUERY_THRESHOLD}\G"
 LIST=`echo "$QUERY" | mysql -u root -p${MYSQL_PASSWORD}`
 QUERYCOUNT=`echo "$LIST" | sed /^$/d | wc -l`
 
 if [ $QUERYCOUNT -gt 0 ] ; then
-	QUERYCOUNT=`expr $QUERYCOUNT - 1`
-	echo "Slow queries found ($QUERYCOUNT)" 1>&2
-	echo "$QUERY"
-	echo "$LIST"
-	exit 1
+        echo "Slow queries found"
+        echo "$QUERY" | tr -d '\r'
+        echo "$LIST" | tr -d '\r'
+        exit 1
 else
-	echo "No slow query found"
-	exit 0
+        echo "No slow query found"
+        exit 0
 fi
 exit 0
